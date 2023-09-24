47af668eac52b367ac826710f511ac78aa7b27ef
Shreyas S Kasetty <shreyasskasetty@MacBook-Pro-58.local>
2023-09-24 11:33:06 -0500

    add new movie in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,9 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
+    {:title => 'Perks of being a Wallflower', :rating => 'PG-13',
+      :release_date => '21-Sep-2012'}
   ]
   
   more_movies.each do |movie|
